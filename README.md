eb354a2715171d2955e47784fa029e771b8446ba
Lorenzo Rovigatti <lorenzo.rovigatti@gmail.com>
2022-12-26 23:16:40 +0100

    Add the possibility of running valgrind if the execution fails

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pynta
+
+## Acknowledgements
+
+* The parser that handles valgrind's output has been taken from [https://github.com/siddheshsathe/Valgrind-Log-Parser](Valgrind-Log-Parser)
--- pynta/pynta.py
+++ pynta/pynta.py
@@ -5,6 +5,7 @@ Created on Dec 25, 2022
 '''
 
 import sys, os, re
+import signal
 from dataclasses import dataclass
 import subprocess as sp
 
@@ -46,13 +47,18 @@ class Compiler:
             
         if "all_warnings_command" in options:
             Compiler.all_warnings_command = options["all_warnings_command"]
+
+        if options["debug_symbols"]:
+            Compiler.command += " -g2"
+            Compiler.all_warnings_command += " -g2"
         
         self.warnings = list()
         self.all_warnings = list()
         self.errors = list()
         
         self.source_file = options["filename"]
-        self.exe_file = os.path.splitext(self.source_file)[0]
+        exe_name = os.path.splitext(self.source_file)[0]
+        self.exe_file = os.path.join(os.getcwd(), exe_name)
         self.compile()
 
     def _severity(self, message):
@@ -82,7 +88,6 @@ class Compiler:
     
     def _entry_linker(self, m):
         file_ = m.group(1).strip()
-        #lineno = m.group(2)
         severity = self._severity(m.group(3))
         message = m.group(3) + m.group(4)
         return Entry(file_, severity, message)
@@ -104,13 +109,8 @@ class Compiler:
         return None
         
     def _invoke_compiler(self, command):
-        try:
-            result = sp.run(command.split() + ["-o", self.exe_file, self.source_file], stdout=sp.PIPE, stderr=sp.STDOUT, text=True)
-        except sp.CalledProcessError as e:
-            print(e.stdout)
-            return False
-        else:
-            return result.returncode, result.stdout
+        result = sp.run(command.split() + ["-o", self.exe_file, self.source_file], stdout=sp.PIPE, stderr=sp.STDOUT, text=True)
+        return result.returncode, result.stdout
         
     def compiled(self):
         return self.regular_return_code == 0
@@ -133,9 +133,83 @@ class Compiler:
                 entry = self._entry_from_line(line)
                 if entry is not None and entry.severity == "warning":
                     self.all_warnings.append(entry)
+                    
+    def write_report(self, filename):
+        with open(filename, "w") as f:
+            if self.compiled():
+                print("--> COMPILATION SUCCESSFUL <--", file=f)
+            else:
+                print("--> COMPILATION FAILED <--", file=f)
+            print("", file=f)
+            
+            print("------------------------", file=f)
+            print(f"OUTPUT FOR '{self.command}'", file=f)
+            print("------------------------\n", file=f)
+            for e in self.errors:
+                print(e, file=f)
+    
+            for w in self.warnings:
+                print(w, file=f)
+        
+            if options["all_warnings"]:
+                print("\n------------------------", file=f)
+                print(f"OUTPUT FOR '{self.all_warnings_command}'", file=f)
+                print("------------------------\n", file=f)
+                
+                for w in self.all_warnings:
+                    print(w, file=f)
     
 
-class FileAnalyser:
+class Launcher:
+    def __init__(self, options, exe_file):
+        self.options = options
+        self.exe_file = exe_file
+        self.arguments = self.options["arguments"].split()
+        self.command = [self.exe_file] + self.arguments
+        
+        self.execute()
+        
+    def success(self):
+        return self.return_code == 0
+
+    def execute(self):
+        result = sp.run(self.command, stdout=sp.PIPE, stderr=sp.PIPE, text=True)
+        
+        self.return_code = result.returncode
+        self.stdout = result.stdout
+        self.stderr = result.stderr
+        
+    def write_report(self, filename):
+        with open(filename, "w") as f:
+            if self.success():
+                print("--> EXECUTION SUCCESSFUL <--\n", file=f)
+                
+                print("------------------------", file=f)
+                print(f"STANDARD OUTPUT", file=f)
+                print("------------------------\n", file=f)
+                print(self.stdout, file=f)
+                
+                print("------------------------", file=f)
+                print(f"STANDARD ERROR", file=f)
+                print("------------------------\n", file=f)
+                print(self.stderr, file=f)
+            else:
+                print("--> EXECUTION FAILED <--\n", file=f)
+                
+                print(f"Return code: {self.return_code}", file=f)
+                if self.return_code == -signal.SIGSEGV:
+                    print(f"Error type: SEGMENTATION FAULT (SIGSEGV)", file=f)
+                elif self.return_code == -signal.SIGABRT:
+                    print(f"Error type: SIGABRT", file=f)
+                    
+                if options["valgrind"]:
+                    command = f"{self.options['valgrind_path']} --log-file={self.options['valgrind_log_file']} {self.exe_file}".split()
+                    sp.run(command, stdout=sp.PIPE, stderr=sp.PIPE, text=True)
+                    
+                    print(f"\nThe output of valgrind has been printed to '{self.options['valgrind_log_file']}'", file=f)
+                    
+
+class Analyser:
     def __init__(self):
         pass
 
@@ -143,8 +217,18 @@ class FileAnalyser:
 class Input(dict):
     required = ["filename"]
     booleans = ["all_warnings"]
+    defaults = {
+        "all_warnings" : True,
+        "debug_symbols" : True,
+        "arguments" : "",
+        "valgrind" : True,
+        "valgrind_path" : "valgrind",
+        "valgrind_log_file" : "valgrind_log.txt"
+    }
     
     def __init__(self, input_file):
+        self.update(Input.defaults)
+        
         self.input_file = input_file
         
         with open(input_file) as f:
@@ -186,28 +270,9 @@ if __name__ == '__main__':
         
     options = Input(sys.argv[1])
     compiler = Compiler(options)
+    compiler.write_report("compilation_report.txt")
     
-    with open("compilation_results.txt", "w") as f:
-        if compiler.compiled():
-            print("--> COMPILATION SUCCESSFUL <--", file=f)
-        else:
-            print("--> COMPILATION FAILED <--", file=f)
-        print("", file=f)
-        
-        print("------------------------", file=f)
-        print(f"OUTPUT FOR '{compiler.command}'", file=f)
-        print("------------------------\n", file=f)
-        for e in compiler.errors:
-            print(e, file=f)
-
-        for w in compiler.warnings:
-            print(w, file=f)
-    
-        if "all_warnings" in options and options["all_warnings"]:
-            print("\n------------------------", file=f)
-            print(f"OUTPUT FOR '{compiler.all_warnings_command}'", file=f)
-            print("------------------------\n", file=f)
-            
-            for w in compiler.all_warnings:
-                print(w, file=f)
+    if compiler.compiled():
+        launcher = Launcher(options, compiler.exe_file)
+        launcher.write_report("execution_report.txt")
     
